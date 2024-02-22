eba7b7b810b840005aeceb36761c05041193ee30
Vasundhara <85097331+vvasundharaa@users.noreply.github.com>
2024-02-22 21:28:16 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,17 @@
 function redirectToNextPage() {
   
-    window.location.href = 'quiz.html';
+    window.location.href = 'quizform.html';
   }
   
+
+$(document).ready(function() {
+    $('#subscribeForm').submit(function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        // Simulate form submission success or failure
+        var isSuccess = Math.random() < 0.5; // Simulate 50% success rate
+        if (isSuccess) {
+        $('#successMessage').modal('show'); // Show success modal
+        } 
+    });
+});    
